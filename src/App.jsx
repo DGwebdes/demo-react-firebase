3423e82e6dfa545bf77c2ac82781cdce78bdf27d
DGWebdev2 <alan.bilik8@gmail.com>
2026-05-29 14:48:29 +0000

    migrating slowly, firebase out

--- src/App.jsx
+++ src/App.jsx
@@ -5,56 +5,51 @@ import { db } from "./config/firebaseConfig"; // Import Firebase Firestore
 import Posts from "./pages/Posts";
 import PostDetail from "./components/PostDetail";
 import CreatePost from "./components/CreatePost";
-import { AuthProvider } from "./context/AuthContext";
+// import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 function App() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const querySnapshot = await getDocs(collection(db, "posts"));
-            setPosts(
-                querySnapshot.docs.map((doc) => {
-                    const data = doc.data();
-                    return {
-                        id: doc.id,
-                        title: data.title,
-                        content: data.content,
-                        date: data.date ? data.date : "Unknown Date",
-                        author: data.author ? data.author : "Unknown Author",
-                        imgURL: data.imgURL ? data.imgURL : "/potion.png",
-                        userId: data.userId,
-                    };
-                }),
-            );
-        };
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const querySnapshot = await getDocs(collection(db, "posts"));
+      setPosts(
+        querySnapshot.docs.map((doc) => {
+          const data = doc.data();
+          return {
+            id: doc.id,
+            title: data.title,
+            content: data.content,
+            date: data.date ? data.date : "Unknown Date",
+            author: data.author ? data.author : "Unknown Author",
+            imgURL: data.imgURL ? data.imgURL : "/potion.png",
+            userId: data.userId,
+          };
+        }),
+      );
+    };
 
-        fetchPosts();
-    }, []);
+    fetchPosts();
+  }, []);
 
-    return (
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/posts" element={<Posts posts={posts} />} />
-                    <Route
-                        path="posts/post/:id"
-                        element={
-                            <PostDetail posts={posts} setPosts={setPosts} />
-                        }
-                    />
-                    <Route
-                        path="/create"
-                        element={<CreatePost setPosts={setPosts} />}
-                    />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-            </Router>
-        </AuthProvider>
-    );
+  return (
+    // <AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts posts={posts} />} />
+        <Route
+          path="posts/post/:id"
+          element={<PostDetail posts={posts} setPosts={setPosts} />}
+        />
+        <Route path="/create" element={<CreatePost setPosts={setPosts} />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+    // </AuthProvider>
+  );
 }
 
 export default App;
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,42 +1,42 @@
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { AuthContext } from "./Contexts";
 import { auth } from "../config/firebaseConfig";
 import {
-    onAuthStateChanged,
-    signOut,
-    GoogleAuthProvider,
-    signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 
 export function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            setUser(user);
-            setLoading(false);
-        });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
 
-        return () => unsubscribe();
-    }, []);
+    return () => unsubscribe();
+  }, []);
 
-    const logout = () => {
-        return signOut(auth);
-    };
+  const logout = () => {
+    return signOut(auth);
+  };
 
-    const GoogleSignIn = async () => {
-        const provider = new GoogleAuthProvider();
-        try {
-            await signInWithPopup(auth, provider);
-        } catch (err) {
-            console.log("Google Error occurred, ", err.message);
-        }
-    };
+  const GoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (err) {
+      console.log("Google Error occurred, ", err.message);
+    }
+  };
 
-    return (
-        <AuthContext.Provider value={{ user, logout, GoogleSignIn }}>
-            {!loading && children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, logout, GoogleSignIn }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
