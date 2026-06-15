6fe89ba13c9ef42d86058134a1551bca138b7c78
DGWebdev2 <alan.bilik8@gmail.com>
2026-06-15 18:55:06 +0000

    minor change

--- src/components/ProtectRoutes.jsx
+++ src/components/ProtectRoutes.jsx
@@ -3,7 +3,6 @@ import { useAuth } from "../context/useAuth";
 
 export const ProtectRoutes = ({ children }) => {
   const { user } = useAuth();
-  console.log("Protected");
 
   if (!user) return <Navigate to="/login" replace />;
   return children;
